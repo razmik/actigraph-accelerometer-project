d62bdf17db760443f8519b0685d7c339cced38fa
razmik <razmik89@gmail.com>
2019-12-21 15:17:22 +1100

    update table 1 creation method

--- supervised_learning/analysis/weekly_time_analysis_t1.py
+++ supervised_learning/analysis/weekly_time_analysis_t1.py
@@ -1,11 +1,14 @@
 import pandas as pd
+from os import listdir
+from os.path import join
 
 
-SUBJECT_DETAIL_FILE = 'user_details.xlsx'
-DATA_FILE = "EE_Prediction_subject_level_evaluation_v2.xlsx"
+def load_df(folder_name):
+    results_file = [f for f in listdir(folder_name) if '.csv' in f][0]
+    return pd.read_csv(join(folder_name, results_file))
 
 
-def get_values_60s(df_all):
+def get_PA_in_hours(df_all):
 
     # SB
     sb_reference = "{}#{}".format(round(df_all['actual_SB'].mean() / 60, 1), round(df_all['actual_SB'].std() / 60, 1))
@@ -29,185 +32,119 @@ def get_values_60s(df_all):
     mvpa_cnn60 = "{}#{}".format(round(df_all['predicted_MVPA'].mean() / 60, 1),
                                 round(df_all['predicted_MVPA'].std() / 60, 1))
 
-    return sb_reference, sb_cnn60, lpa_reference, lpa_cnn60, sblpa_reference, sblpa_cnn60, mvpa_reference, mvpa_cnn60
+    # Total
+    total_time = sblpa_reference + mvpa_reference
+
+    return sb_reference, sb_cnn60, lpa_reference, lpa_cnn60, sblpa_reference, sblpa_cnn60, mvpa_reference, mvpa_cnn60, total_time
 
 
-def get_values_30s(df_all):
+def get_PA_in_hours_HBLR(df_all):
 
-    # SB
-    sb_reference = "{}#{}".format(round(df_all['actual_SB'].mean() / (4*60), 1), round(df_all['actual_SB'].std() / (4*60), 1))
-    sb_cnn30 = "{}#{}".format(round(df_all['predicted_SB'].mean() / (4*60), 1), round(df_all['predicted_SB'].std() / (4*60), 1))
-
-    # LPA
-    lpa_reference = "{}#{}".format(round(df_all['actual_LPA'].mean() / (4*60), 1),
-                                   round(df_all['actual_LPA'].std() / (4*60), 1))
-    lpa_cnn30 = "{}#{}".format(round(df_all['predicted_LPA'].mean() / (4*60), 1),
-                               round(df_all['predicted_LPA'].std() / (4*60), 1))
-
-    # SB+LPA
-    sblpa_reference = "{}#{}".format(round((df_all['actual_SB'] + df_all['actual_LPA']).mean() / (4*60), 1),
-                                     round((df_all['actual_SB'] + df_all['actual_LPA']).std() / (4*60), 1))
-    sblpa_cnn30 = "{}#{}".format(round((df_all['predicted_SB'] + df_all['predicted_LPA']).mean() / (4*60), 1),
-                                 round((df_all['predicted_SB'] + df_all['predicted_LPA']).std() / (4*60), 1))
+    # SBLPA
+    sblpa_reference = "{}#{}".format(round(df_all['actual_SBLPA'].mean() / 60, 1), round(df_all['actual_SBLPA'].std() / 60, 1))
+    sblpa_cnn60 = "{}#{}".format(round(df_all['predicted_SBLPA'].mean() / 60, 1), round(df_all['predicted_SBLPA'].std() / 60, 1))
 
     # MVPA
-    mvpa_reference = "{}#{}".format(round(df_all['actual_MVPA'].mean() / (4*60), 1),
-                                    round(df_all['actual_MVPA'].std() / (4*60), 1))
-    mvpa_cnn30 = "{}#{}".format(round(df_all['predicted_MVPA'].mean() / (4*60), 1),
-                                round(df_all['predicted_MVPA'].std() / (4*60), 1))
+    mvpa_reference = "{}#{}".format(round(df_all['actual_MVPA'].mean() / 60, 1),
+                                    round(df_all['actual_MVPA'].std() / 60, 1))
+    mvpa_cnn60 = "{}#{}".format(round(df_all['predicted_MVPA'].mean() / 60, 1),
+                                round(df_all['predicted_MVPA'].std() / 60, 1))
 
-    return sb_reference, sb_cnn30, lpa_reference, lpa_cnn30, sblpa_reference, sblpa_cnn30, mvpa_reference, mvpa_cnn30
+    # Total
+    total_time = sblpa_reference + mvpa_reference
+
+    return sblpa_reference, sblpa_cnn60, mvpa_reference, mvpa_cnn60, total_time
+
+
+def append_data_results(result_data, df_3000_filt, df_6000_filt, df_hblr_filt, title):
+
+    assert df_3000_filt.shape[0] == df_6000_filt.shape[0] == df_hblr_filt.shape[0]
+
+    set_val = '{} (n={})'.format(title, df_3000_filtered.shape[0])
+
+    sb_reference30, sb_cnn30, lpa_reference30, lpa_cnn30, sblpa_reference30, sblpa_cnn30, mvpa_reference30, mvpa_cnn30, total_30 = get_PA_in_hours(
+        df_3000_filt)
+    sb_reference60, sb_cnn60, lpa_reference60, lpa_cnn60, sblpa_reference60, sblpa_cnn60, mvpa_reference60, mvpa_cnn60, total_60 = get_PA_in_hours(
+        df_6000_filt)
+    sblpa_referencehb, sblpa_hb, mvpa_referencebh, mvpa_hb, total_hb = get_PA_in_hours_HBLR(df_hblr_filt)
+
+    result_data.append([set_val, 'SB', sb_reference30, sb_cnn30, sb_reference60, sb_cnn60, '', ''])
+    result_data.append([set_val, 'LPA', lpa_reference30, lpa_cnn30, lpa_reference60, lpa_cnn60, '', ''])
+    result_data.append([set_val, 'SB+LPA', sblpa_reference30, sblpa_cnn30, sblpa_reference60, sblpa_cnn60, sblpa_referencehb, sblpa_hb])
+    result_data.append([set_val, 'MVPA', mvpa_reference30, mvpa_cnn30, mvpa_reference60, mvpa_cnn60, mvpa_referencebh, mvpa_hb])
+
+    return result_data
 
 
 if __name__ == '__main__':
 
-    subject_df = pd.read_excel(SUBJECT_DETAIL_FILE).groupby('Participant ID').first().reset_index()
+    training_version = '1-12_Dec'
+    eval_category = ['train_test', 'test']
 
-    result_data = []
-    result_col_names = ['Set', 'PA category', 'Reference', 'cnn_60']
+    SUBJECT_DETAIL_FILE = 'user_details.xlsx'
+    HBLR_ROOT = "E:/Projects/Accelerometer-project_Rashmika/supervised_learning/hb-lr-eval/outputs"
+    CNN_ROOT = "E:/Projects/Accelerometer-project_Rashmika/supervised_learning/output/v{}_Incorrect".format(training_version)
 
-    data_df = pd.read_excel(DATA_FILE, sheet_name='unique_participants_wk2_60s')
-    df_summary = pd.merge(data_df, subject_df, left_on='participant', right_on='Participant ID', how='left')
+    participant_df = pd.read_excel(SUBJECT_DETAIL_FILE).groupby('Participant ID').first().reset_index()
 
-    # All
-    df_filt = df_summary[:]
-    set_val = 'All (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn60, lpa_reference, lpa_cnn60, sblpa_reference, sblpa_cnn60, mvpa_reference, mvpa_cnn60 = get_values_60s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn60])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn60])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn60])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn60])
+    result_col_names = ['Set', 'PA category', 'Reference_cnn30', 'cnn_30', 'Reference_cnn60', 'cnn_60', 'Reference_hblr', 'hblr']
 
-    # Male
-    df_filt = df_summary.loc[df_summary['gender'] == 'Male']
-    set_val = 'Male (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn60, lpa_reference, lpa_cnn60, sblpa_reference, sblpa_cnn60, mvpa_reference, mvpa_cnn60 = get_values_60s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn60])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn60])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn60])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn60])
+    for data_cat in eval_category:
 
-    # Female
-    df_filt = df_summary.loc[df_summary['gender'] == 'Female']
-    set_val = 'Female (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn60, lpa_reference, lpa_cnn60, sblpa_reference, sblpa_cnn60, mvpa_reference, mvpa_cnn60 = get_values_60s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn60])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn60])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn60])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn60])
+        class_3000_df = load_df(join(CNN_ROOT, 'classification', 'window-3000-overlap-1500', 'individual_results', data_cat))
+        df_3000_summary = pd.merge(class_3000_df, participant_df, left_on='participant', right_on='Participant ID', how='left')
 
-    # Other
-    df_filt = df_summary.loc[df_summary['gender'] == 'Other']
-    set_val = 'Other (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn60, lpa_reference, lpa_cnn60, sblpa_reference, sblpa_cnn60, mvpa_reference, mvpa_cnn60 = get_values_60s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn60])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn60])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn60])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn60])
+        class_6000_df = load_df(join(CNN_ROOT, 'classification', 'window-6000-overlap-3000', 'individual_results', data_cat))
+        df_6000_summary = pd.merge(class_6000_df, participant_df, left_on='participant', right_on='Participant ID', how='left')
 
-    # Normal
-    df_filt = df_summary.loc[df_summary['bmi'] < 25]
-    set_val = 'Normal (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn60, lpa_reference, lpa_cnn60, sblpa_reference, sblpa_cnn60, mvpa_reference, mvpa_cnn60 = get_values_60s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn60])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn60])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn60])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn60])
+        class_hblr_df = load_df(join(HBLR_ROOT, data_cat, 'individual'))
+        df_hblr_summary = pd.merge(class_hblr_df, participant_df, left_on='participant', right_on='Participant ID', how='left')
 
-    # Overweight
-    df_filt = df_summary.loc[(25 <= df_summary['bmi']) & (df_summary['bmi'] < 30)]
-    set_val = 'Overweight (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn60, lpa_reference, lpa_cnn60, sblpa_reference, sblpa_cnn60, mvpa_reference, mvpa_cnn60 = get_values_60s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn60])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn60])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn60])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn60])
+        result_data = []
 
-    # Obese
-    df_filt = df_summary.loc[30 <= df_summary['bmi']]
-    set_val = 'Obese (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn60, lpa_reference, lpa_cnn60, sblpa_reference, sblpa_cnn60, mvpa_reference, mvpa_cnn60 = get_values_60s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn60])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn60])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn60])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn60])
+        # All Participants
+        df_3000_filtered = df_3000_summary[:]
+        df_6000_filtered = df_6000_summary[:]
+        df_hblr_filtered = df_hblr_summary[:]
+        result_data = append_data_results(result_data, df_3000_filtered, df_6000_filtered, df_hblr_filtered, 'All')
 
-    #Save
-    pd.DataFrame(result_data, columns=result_col_names).to_csv('weekly_analysis_60s.csv', index=None)
+        # Male
+        df_3000_filtered = df_3000_summary.loc[df_3000_summary['gender'] == 'Male']
+        df_6000_filtered = df_6000_summary.loc[df_6000_summary['gender'] == 'Male']
+        df_hblr_filtered = df_hblr_summary.loc[df_hblr_summary['gender'] == 'Male']
+        result_data = append_data_results(result_data, df_3000_filtered, df_6000_filtered, df_hblr_filtered, 'Male')
 
+        # Female
+        df_3000_filtered = df_3000_summary.loc[df_3000_summary['gender'] == 'Female']
+        df_6000_filtered = df_6000_summary.loc[df_6000_summary['gender'] == 'Female']
+        df_hblr_filtered = df_hblr_summary.loc[df_hblr_summary['gender'] == 'Female']
+        result_data = append_data_results(result_data, df_3000_filtered, df_6000_filtered, df_hblr_filtered, 'Female')
 
+        # Other
+        df_3000_filtered = df_3000_summary.loc[df_3000_summary['gender'] == 'Other']
+        df_6000_filtered = df_6000_summary.loc[df_6000_summary['gender'] == 'Other']
+        df_hblr_filtered = df_hblr_summary.loc[df_hblr_summary['gender'] == 'Other']
+        result_data = append_data_results(result_data, df_3000_filtered, df_6000_filtered, df_hblr_filtered, 'Other')
 
+        # Normal
+        df_3000_filtered = df_3000_summary.loc[df_3000_summary['bmi'] < 25]
+        df_6000_filtered = df_6000_summary.loc[df_6000_summary['bmi'] < 25]
+        df_hblr_filtered = df_hblr_summary.loc[df_hblr_summary['bmi'] < 25]
+        result_data = append_data_results(result_data, df_3000_filtered, df_6000_filtered, df_hblr_filtered, 'Normal')
 
-    result_data = []
-    result_col_names = ['Set', 'PA category', 'Reference', 'cnn_30']
+        # Overweight
+        df_3000_filtered = df_3000_summary.loc[(25 <= df_3000_summary['bmi']) & (df_3000_summary['bmi'] < 30)]
+        df_6000_filtered = df_6000_summary.loc[(25 <= df_6000_summary['bmi']) & (df_6000_summary['bmi'] < 30)]
+        df_hblr_filtered = df_hblr_summary.loc[(25 <= df_hblr_summary['bmi']) & (df_hblr_summary['bmi'] < 30)]
+        result_data = append_data_results(result_data, df_3000_filtered, df_6000_filtered, df_hblr_filtered, 'Overweight')
 
-    data_df = pd.read_excel(DATA_FILE, sheet_name='unique_participants_wk2_30s')
-    df_summary = pd.merge(data_df, subject_df, left_on='participant', right_on='Participant ID', how='left')
+        # Obese
+        df_3000_filtered = df_3000_summary.loc[30 <= df_3000_summary['bmi']]
+        df_6000_filtered = df_6000_summary.loc[30 <= df_6000_summary['bmi']]
+        df_hblr_filtered = df_hblr_summary.loc[30 <= df_hblr_summary['bmi']]
+        result_data = append_data_results(result_data, df_3000_filtered, df_6000_filtered, df_hblr_filtered, 'Obese')
 
-    # All
-    df_filt = df_summary[:]
-    set_val = 'All (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn30, lpa_reference, lpa_cnn30, sblpa_reference, sblpa_cnn30, mvpa_reference, mvpa_cnn30 = get_values_30s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn30])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn30])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn30])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn30])
-
-    # Male
-    df_filt = df_summary.loc[df_summary['gender'] == 'Male']
-    set_val = 'Male (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn30, lpa_reference, lpa_cnn30, sblpa_reference, sblpa_cnn30, mvpa_reference, mvpa_cnn30 = get_values_30s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn30])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn30])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn30])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn30])
-
-    # Female
-    df_filt = df_summary.loc[df_summary['gender'] == 'Female']
-    set_val = 'Female (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn30, lpa_reference, lpa_cnn30, sblpa_reference, sblpa_cnn30, mvpa_reference, mvpa_cnn30 = get_values_30s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn30])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn30])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn30])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn30])
-
-    # Other
-    df_filt = df_summary.loc[df_summary['gender'] == 'Other']
-    set_val = 'Other (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn30, lpa_reference, lpa_cnn30, sblpa_reference, sblpa_cnn30, mvpa_reference, mvpa_cnn30 = get_values_30s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn30])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn30])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn30])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn30])
-
-    # Normal
-    df_filt = df_summary.loc[df_summary['bmi'] < 25]
-    set_val = 'Normal (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn30, lpa_reference, lpa_cnn30, sblpa_reference, sblpa_cnn30, mvpa_reference, mvpa_cnn30 = get_values_30s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn30])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn30])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn30])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn30])
-
-    # Overweight
-    df_filt = df_summary.loc[(25 <= df_summary['bmi']) & (df_summary['bmi'] < 30)]
-    set_val = 'Overweight (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn30, lpa_reference, lpa_cnn30, sblpa_reference, sblpa_cnn30, mvpa_reference, mvpa_cnn30 = get_values_30s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn30])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn30])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn30])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn30])
-
-    # Obese
-    df_filt = df_summary.loc[30 <= df_summary['bmi']]
-    set_val = 'Obese (n={})'.format(df_filt.shape[0])
-    sb_reference, sb_cnn30, lpa_reference, lpa_cnn30, sblpa_reference, sblpa_cnn30, mvpa_reference, mvpa_cnn30 = get_values_30s(df_filt)
-    result_data.append([set_val, 'SB', sb_reference, sb_cnn30])
-    result_data.append([set_val, 'LPA', lpa_reference, lpa_cnn30])
-    result_data.append([set_val, 'SB+LPA', sblpa_reference, sblpa_cnn30])
-    result_data.append([set_val, 'MVPA', mvpa_reference, mvpa_cnn30])
-
-    #Save
-    pd.DataFrame(result_data, columns=result_col_names).to_csv('weekly_analysis_30s.csv', index=None)
+        # Save table 1 output
+        pd.DataFrame(result_data, columns=result_col_names).to_csv('Table_1_Weekly_Analysis_{}_{}.csv'.format(training_version, data_cat), index=None)
 
     print('Completed')
